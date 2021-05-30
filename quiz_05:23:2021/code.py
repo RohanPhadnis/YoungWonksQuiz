458bae0f414bb6808d33f208154e26039cc91449
RohanPhadnis <59270766+RohanPhadnis@users.noreply.github.com>
2021-05-30 13:59:38 -0700

    quiz may 30

--- quiz_05:23:2021/code.py
+++ quiz_05:23:2021/code.py
@@ -4,14 +4,20 @@ with open('data.csv') as file:
     text = file.read().split('\n')
 
 keys = text[0].split(',')
+print(keys)
 l = []
 
 for val in text[1:]:
+    
     if len(val) > 0:
         d = {}
         data = val.split(',')
+        print(data)
         for k in range(len(keys)):
-            d[keys[k]] = data[k]
+            try:
+                d[keys[k]] = float(data[k])
+            except:
+                d[keys[k]] = data[k]
         l.append(d)
 
 pprint.pprint(l)
---
+++ quiz_05:30:2021/code.py
@@ -0,0 +1,31 @@
+l = []
+TAX_RATE = 0.3
+
+with open('data.csv') as file:
+    text = file.read().split('\n')
+
+headers = text[0].split(',')
+
+for data in text[1:]:
+    temp = data.split(',')
+    d = {}
+    for n in range(len(temp)):
+        d[headers[n]] = temp[n]
+    l.append(d)
+
+for n in range(len(l)):
+    for k in headers:
+        if k not in l[n].keys():
+            if k == 'tax':
+                l[n]['tax'] = str(int(TAX_RATE * int(l[n]['pay'])))
+            elif k == 'total':
+                l[n]['total'] = str(int((1 - TAX_RATE) * int(l[n]['pay'])))
+
+write = ''
+
+s = ','.join(headers) + '\n'
+for d in l:
+    s += ','.join(d.values()) + '\n'
+
+with open('data.csv', 'w') as file:
+    file.write(s)
